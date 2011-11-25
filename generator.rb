4cd915d0e0efe6c061635933f79f9b4b9a7c8d77
Tim Blair <trblair@gmail.com>
2011-11-25 15:16:57 -0800

    Golf version of countdown.

---
+++ generator.rb
@@ -0,0 +1,35 @@
+require_relative 'countdown'
+def generate(small, large)
+	larges = [25, 50, 75, 100]
+	smalls = []
+	(1...10).each do |i|
+		smalls << i
+		smalls << i
+	end
+
+	results = []
+	smalls.shuffle!
+	small.times do
+		results << smalls.slice!(0)
+	end
+
+	larges.shuffle!
+	large.times do
+		results << larges.slice!(0)
+	end
+
+	results
+end
+
+puts 'How many smalls?'
+resp = gets
+smalls = resp.to_i
+
+n = generate(smalls, 6 - smalls)
+t = Random.new.rand(100..1000)
+
+puts "The numbers are: #{n}"
+puts "The target is: #{t}"
+
+result, method = h(t, n)
+puts "Best result is #{result} via #{method}"
