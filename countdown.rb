9ce9c24ff20434a788f57daf097c0449b5f07d1f
Tim Blair <trblair@gmail.com>
2011-11-25 16:33:45 -0800

    Got to sub 400 chars :) I have some concern that this is no longer correct for 1 edge case

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,17 @@
 def h t,n
+f=''
 d=t
 m={}
 u=%w(+ - * /)
 b={}
-n.each{|i|return i if i==t
-b[[i]]=[i]
+n.each{|i|b[[i]]=[i]
 m[[i,[i]]]=i}
-(2..n.size).each{|i|n.permutation(i).to_a.uniq.each{|p|(1..p.size-1).each{|s|l=p.slice(0...s)
-a=p.slice(s..-1)
+(2..n.size).each{|i|n.permutation(i).to_a.uniq.each{|p|(1..p.size-1).each{|s|l=p.slice 0...s
+a=p.slice s..-1
 o=[]
 b[l].each{|q|u.each{|k|b[a].each{|g|next if k=='/'&&q%g<1
-c=q.send(k,g)
-next if c==0
+c=q.send k,g
+next if c<1
 o=o|[c]
 m[[c,p]]="(#{m[[q,l]]})#{k}(#{m[[g,a]]})"
 e=(c-t).abs
