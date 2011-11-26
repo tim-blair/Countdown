5c45279af4b227836193e7a692ab5e33a95706a4
Tim Blair <trblair@gmail.com>
2011-11-25 16:02:12 -0800

    get rid of sets, since arrays can do uniqueness

--- countdown.rb
+++ countdown.rb
@@ -1,19 +1,18 @@
-require'set'
 def h t,n
 d=t
 m={}
 u=%w(+ - * /)
 b={}
 n.each{|i|return i if i==t
-b[[i]]=Set.new [i]
+b[[i]]=[i]
 m[[i,[i]]]=i}
 (2..n.size).each{|i|n.permutation(i).to_a.uniq.each{|p|(1..p.size-1).each{|s|l=p.slice(0...s)
 a=p.slice(s..-1)
-o=Set.new
+o=[]
 b[l].each{|q|u.each{|k|b[a].each{|g|next if k=='/'&&q%g<1
 c=q.send(k,g)
 next if c==0
-o.add(c)
+o=o|[c]
 m[[c,p]]="(#{m[[q,l]]})#{k}(#{m[[g,a]]})"
 e=(c-t).abs
 if e<d
